c87752319707e2a5590dd47ee6bfa4803f3dec37
Cadier Antoine <antoine.cadier@etu.univ-amu.fr>
2023-12-29 22:00:05 +0100

    Added loop to the menu

--- main.cpp
+++ main.cpp
@@ -6,40 +6,42 @@ int main(){
     agenda newagenda;
     event newevent;
     loadagenda(newagenda);
-    showmenu();
-    int pressed = getreply();
-    switch (pressed)
-    {
-    case 1:
-        modifytitle(newagenda);
-        modifydescription(newagenda);
-        break;
-    case 2:
-        displayagenda(newagenda);
-        break;
-    case 3:
-        setid(newevent);
-        modifytitle(newevent);
-        modifydescription(newevent);
-        setstartdate(newevent);
-        setenddate(newevent);
-        newagenda.events.push_back(newevent);
-        break;
-    case 4:
+    while (true){
+        showmenu();
+        int pressed = getreply();
+        switch (pressed)
+        {
+        case 1:
+            modifytitle(newagenda);
+            modifydescription(newagenda);
+            break;
+        case 2:
+            displayagenda(newagenda);
+            break;
+        case 3:
+            setid(newevent);
+            modifytitle(newevent);
+            modifydescription(newevent);
+            setstartdate(newevent);
+            setenddate(newevent);
+            newagenda.events.push_back(newevent);
+            break;
+        case 4:
 
-        break;
-    case 5:
+            break;
+        case 5:
 
-        break;
-    case 6:
+            break;
+        case 6:
 
-        break;
-    case 7:
-        return 0;
-        break;
-    default:
-        std::cout<<"cancel"<<std::endl;
-        break;
+            break;
+        case 7:
+            return 0;
+            break;
+        default:
+            std::cout<<"cancel"<<std::endl;
+            break;
+        }
     }
-    return 0;
+return 0;
 }
