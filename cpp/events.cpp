00c9174603c28382fc6519b50cc515af537102e9
Cadier Antoine <antoine.cadier@etu.univ-amu.fr>
2023-12-29 14:24:46 +0100

    Adds events

---
+++ cpp/events.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include "../hpp/events.hpp"
+
+std::string getUID(std::size_t len){
+    std::vector<char> x = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a','b', 'c', 'd', 'e', 'f'};
+    std::string uid(len, '\0');
+    static std::mt19937 gen(std::random_device{}());
+    std::uniform_int_distribution < > dis(0, x.size()-1);
+    for (char& c: uid) {
+        c = x[dis(gen)];
+    }
+    return uid;
+}
+
+std::string askdesc(std::string what){
+    std::string result = "";
+    std::cout<<"Entrer le nouveau "<<what<<" de l'événement."<<std::endl;
+    if(what != "titre"){
+    do{
+    std::cin>>result;
+    }while (result != ".");}
+    else std::cin>>result;
+    return result;
+}
+
+void changetitle(std::string title, event &tomodify){
+    tomodify.title = title;
+}
+
+void modifytitle(event &tomodify){
+    changetitle(askdesc("titre"), tomodify);
+}
+
+void changedescription(std::string description , event &tomodify){
+    tomodify.description = description;
+}
+
+void modifydescription(event &tomodify){
+    changedescription(askdesc("description"), tomodify);
+}
+
---
+++ hpp/events.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ctime>
+
+struct event{
+    char id[8];
+    std::string title;
+    std::string description;
+    std::tm startdate;
+    std::tm enddate;
+    
+};
+
